855ac049fee5c12d7807ee687aa45b7f4fb88380
Froad1 <47084307+Froad1@users.noreply.github.com>
2022-10-18 22:37:03 +0200

    Update js-app.3783fa46.js

--- js/js-app.3783fa46.js
+++ js/js-app.3783fa46.js
@@ -1418,7 +1418,7 @@
         function un(e, t) {
             var n = Object(a["resolveComponent"])("router-link");
             return Object(a["openBlock"])(), Object(a["createElementBlock"])("div", sn, [dn, Object(a["createVNode"])(n, {
-                to: "/"
+                to: "/404"
             }, {
                 default: Object(a["withCtx"])((function() {
                     return [mn]
